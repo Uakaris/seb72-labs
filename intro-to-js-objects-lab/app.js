897425065c334eecd94656d04f7e5dc2e8fe5782
Charlie Jay <charliejbjay@gmail.com>
2024-05-23 13:14:31 +1000

    Completed exercise 8

--- intro-to-js-objects-lab/app.js
+++ intro-to-js-objects-lab/app.js
@@ -44,7 +44,7 @@ Exercise 4
 Solve Exercise 4 here:
 */
 
-game.party.push(pokemon[4]);
+game.party.push(pokemon[24]);
 
 /*
 Exercise 5
@@ -55,7 +55,7 @@ Exercise 5
 Solve Exercise 5 here:
 */
 
-game.party.push(pokemon[100], pokemon[75], pokemon[30]);
+game.party.push(pokemon[0], pokemon[4], pokemon[6]);
 
 //['Pikachu', 'Mewtwo', 'Ninetales', 'Machamp'];
 
@@ -90,3 +90,23 @@ game.gyms.forEach(gym => {
         gym.completed = true;
     }
 });
+
+/*
+Exercise 8
+1. Evolve the starter Pokémon you added to your party earlier. Each starter Pokémon evolves into a specific one.
+2. How would you replace the current starter Pokémon in your party with its evolved form?
+
+Hint: 
+  - Pokemon 1: Bulbasaur evolves into Pokemon 2: Ivysaur
+  - Pokemon 4: Charmander evolves into Pokemon 5: Charmeleon
+  - Pokemon 7: Squirtle evolves into Pokemon 8: Wartortle
+  - Pokemon 25: Pikachu evolves into Pokemon 26: Raichu
+
+More Hints: The existing starter Pokemon will be *replaced* in your party with the Pokemon it evolved into. Remember that you're working with an array of objects - what array method is ideal for replacing one element with another? 
+
+Solve Exercise 8 here:
+*/
+
+game.party.splice(0, 4, pokemon[1], pokemon[4], pokemon[7], pokemon[25]);
+
+console.log(game.party);
