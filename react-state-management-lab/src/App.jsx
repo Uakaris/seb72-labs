4f480b047a2fbc2609faf7093476c93ddec019fd
Charlie Jay <charliejbjay@gmail.com>
2024-07-02 18:59:28 +1000

    first push

---
+++ react-state-management-lab/src/App.jsx
@@ -0,0 +1,114 @@
+import { useState } from 'react';
+
+const ZombieFighter = (props) => {
+  const { name, price, strength, agility, img } = props;
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <h3>{ name }</h3>
+          <h4>{ price }</h4>
+          <h4>{ strength }</h4>
+          <h4>{ agility }</h4>
+          <img src={ img }></img>
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+  const [team] = useState([]);
+  const [money] = useState(100);
+  const [zombieFighters] = useState(
+    [
+      {
+        name: 'Survivor',
+        price: 12,
+        strength: 6,
+        agility: 4,
+        img: 'https://via.placeholder.com/150/92c952',
+      },
+      {
+        name: 'Scavenger',
+        price: 10,
+        strength: 5,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/771796',
+      },
+      {
+        name: 'Shadow',
+        price: 18,
+        strength: 7,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/24f355',
+      },
+      {
+        name: 'Tracker',
+        price: 14,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/d32776',
+      },
+      {
+        name: 'Sharpshooter',
+        price: 20,
+        strength: 6,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/1ee8a4',
+      },
+      {
+        name: 'Medic',
+        price: 15,
+        strength: 5,
+        agility: 7,
+        img: 'https://via.placeholder.com/150/66b7d2',
+      },
+      {
+        name: 'Engineer',
+        price: 16,
+        strength: 6,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/56acb2',
+      },
+      {
+        name: 'Brawler',
+        price: 11,
+        strength: 8,
+        agility: 3,
+        img: 'https://via.placeholder.com/150/8985dc',
+      },
+      {
+        name: 'Infiltrator',
+        price: 17,
+        strength: 5,
+        agility: 9,
+        img: 'https://via.placeholder.com/150/392537',
+      },
+      {
+        name: 'Leader',
+        price: 22,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/602b9e',
+      },
+    ]    
+  )
+
+  return (
+    <>
+    <h1>Zombie Fighters</h1>
+    <div>
+      {zombieFighters.map((zombieFighter) => (
+        <ZombieFighter
+        key={zombieFighter.id}
+        {...zombieFighter}
+        />
+      ))}
+    </div>
+    </>
+  );
+}
+
+export default App
